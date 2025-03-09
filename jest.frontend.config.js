1a3ac0fcf311a8fc25d487f7737d420fc05f9142
Bearypop <looyl2k@gmail.com>
2025-03-09 09:09:51 +0800

    Resolve conflict

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 module.exports = {
-=======
-export default {
->>>>>>> 5d533e8109c8abde0c8234ff427ed51494de2617
   // name displayed during tests
   displayName: "frontend",
 
@@ -24,7 +20,6 @@ export default {
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 
   // only run these tests
-<<<<<<< HEAD
   testMatch: [
     "<rootDir>/client/src/pages/Auth/*.test.js",
     "<rootDir>/client/src/pages/admin/*.test.js",
@@ -32,7 +27,8 @@ export default {
     "<rootDir>/client/src/pages/HomePage.test.js",
     "<rootDir>/client/src/pages/ProductDetails.test.js",
     "<rootDir>/client/src/components/*.test.js",
-    "<rootDir>/client/src/components/Form/*.test.js"
+    "<rootDir>/client/src/components/Form/*.test.js",
+    "<rootDir>/client/src/**/*.test.js"
   ],
 
   // jest code coverage
@@ -44,22 +40,12 @@ export default {
     "client/src/components/AdminMenu.js",
     "client/src/pages/Contact.js",
     "client/src/pages/HomePage.js",
-    "client/src/pages/ProductDetails.js"],
-  coverageThreshold: {
-    global: {
-      lines: 100,
-      functions: 100,
-=======
-  testMatch: ["<rootDir>/client/src/**/*.test.js"],
-
-  // jest code coverage
-  collectCoverage: true,
-  collectCoverageFrom: ["client/src/**/*.{js,jsx}", "!client/src/_site/**"],
+    "client/src/pages/ProductDetails.js",
+    "client/src/**/*.{js,jsx}", "!client/src/_site/**"],
   coverageThreshold: {
     global: {
       lines: 0, // update as test coverage increases
       functions: 0, // update as test coverage increases
->>>>>>> 5d533e8109c8abde0c8234ff427ed51494de2617
     },
   },
 };
